1248f2055f2e29be0568681d0da1eabbafadaf63
Jared Lumpe <mjlumpe@gmail.com>
2021-11-28 20:02:38 -0700

    Convert XML data to Python

--- entrez_tools/common.py
+++ entrez_tools/common.py
@@ -1,4 +1,58 @@
 import re
+from typing import List
+from warnings import warn
+
+from Bio.Entrez import Parser
+
+
+def _check_xml_attrs(value, strict, path):
+	if value.attributes:
+		path_str = '/'.join(map(str, path))
+		msg = f'{type(value)} at {path_str} has attributes.'
+		if strict:
+			raise ValueError(msg)
+		else:
+			warn(msg)
+
+def xml_to_builtin(value, strict_attrs=False, _path=()):
+	"""Recursively convert parsed XML value to builtin Python value.
+
+	Result is similar to parsed JSON. Assumes XML elements do not have any attributes.
+
+	Parameters
+	----------
+	value
+		Value to convert.
+	strict_attrs
+		If True raise error when element has attributes, otherwise just warn.
+	"""
+	if isinstance(value, Parser.NoneElement):
+		_check_xml_attrs(value, strict_attrs, _path)
+		return None
+
+	if isinstance(value, Parser.IntegerElement):
+		_check_xml_attrs(value, strict_attrs, _path)
+		return int(value)
+
+	if isinstance(value, Parser.StringElement):
+		_check_xml_attrs(value, strict_attrs, _path)
+		return str(value)
+
+	if isinstance(value, Parser.ListElement):
+		_check_xml_attrs(value, strict_attrs, _path)
+		return [
+			xml_to_builtin(x, strict_attrs, (*_path, i))
+			for i, x in enumerate(value)
+		]
+
+	if isinstance(value, Parser.DictionaryElement):
+		_check_xml_attrs(value, strict_attrs, _path)
+		return {
+			k: xml_to_builtin(v, strict_attrs, (*_path, k))
+			for k, v in value.items()
+		}
+
+	return value
 
 
 def one_or_none(items, msg: str ='Got more than one result'):
